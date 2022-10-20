d6c8b61ebf858fa978a14973d4851defc6c14e0d
GReeDys666 <48158314+GReeDys666@users.noreply.github.com>
2022-10-20 19:56:17 +0300

    Add files via upload

---
+++ HOMEWORK1/index.js
@@ -0,0 +1,77 @@
+// Task 2
+// console.log("My surname is Ilechko");
+// // END
+
+// // Task 3
+// let firstVariable = 18;
+// let secondVariable = 20;
+
+// alert(`Значення першої змінної = ${firstVariable}, значення другої змінної = ${secondVariable}.`);
+
+// secondVariable = firstVariable;
+
+// alert(`Значення першої змінної = ${firstVariable}, значення другої змінної = ${secondVariable}.`);
+// //END
+
+// // Task 4
+
+// const objectOfData = {
+//     String: "Vasya",
+//     Number: 20,
+//     Boolean: false,
+//     Undefined: undefined,
+//     Null: null
+// }
+// //END
+
+// // Task 5
+
+// let isAdult = confirm("Are you older than 18 y.o. ?");
+// console.log(isAdult);
+
+// // END
+
+// // Task 6
+
+// const myName = "Yuriy";
+// const mySurname = "Ilechko";
+// const myGroupName = "722-JS";
+// const myBirthdayYear = 2000;
+// const married = true;
+
+// console.log(typeof myBirthdayYear);
+// console.log(typeof married);
+// console.log(typeof myName);
+// console.log(typeof mySurname);
+// console.log(typeof myGroupName);
+
+// const zeroVariable  = null;
+// const unAvailable = undefined;
+
+// console.log(typeof zeroVariable);
+// console.log(typeof unAvailable);
+// // END
+
+// // Task 7
+
+// let userName = prompt("Enter your name");
+// let userEmail = prompt("Enter your email");
+// let userPassword = prompt("Enter your password");
+// alert(`Dear ${userName}, your email is ${userEmail}, your password is ${userPassword}`);
+
+// // END
+
+// // Task 8
+
+// const secondsInMinute = 60;
+// const minuteInHour = 60;
+// let secondsOfHour = secondsInMinute * minuteInHour;
+// let secondsOfDay = secondsOfHour * 24;
+// let secondsOfMonth = secondsOfDay * 30;
+
+// alert(`Seconds in hour: ${secondsOfHour}s, seconds in day: ${secondsOfDay}s, seconds in month: ${secondsOfMonth}s`);
+
+// let opinionOfUser = prompt("Please select the option: 1. Hour, 2. Day, 3. Month");
+
+
+// // END
