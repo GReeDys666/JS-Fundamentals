fa3b5a755bef5b85067e2a1343c13c3aed481b26
GReeDys666 <48158314+GReeDys666@users.noreply.github.com>
2022-11-21 14:24:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+$(function () {
+    // // Task 1
+
+    // $('a[href^="https://"]').attr('target','_blank');
+
+    // // END
+
+    // // Task 2
+
+    // $("h2.head").css("background-color","green");
+    // $("h2.head >.inner").css("font-size","35px");
+
+    // // END
+
+    // // Task 3
+    // //not  yet
+    // $('div').insertBefore('h3');
+
+    // // END
+
+    // // Task 4
+
+    // $("input[type=checkbox]").change(function () {
+    //     if ($("input[type=checkbox]:checked").length == 3) {
+    //         $(".group1").attr('disabled', true);
+    //     } else {
+    //         $("type=checkbox").removeAttr('disabled');
+    //     }
+    // })
+
+    // // END
+});
+
